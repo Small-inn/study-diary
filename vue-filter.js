96e7629883d1a030a57f1ea10723b4d6ffe462aa
liuxie <small_inn@163.com>
2019-02-24 17:52:27 +0800

    新增以及修改

---
+++ vue-filter.js
@@ -0,0 +1,42 @@
+/**
+ * 金额加人民币符号,默认两位小数和人民币符号
+ * @param value 数值
+ * @param type 金额字符，默认为￥
+ * @param number 保留几位小数，默认两位
+ */
+import Vue from 'vue'
+// 1.0
+Vue.filter('currency', function(value, type, number) {
+    if (!value) return ''
+    let num = number || 2
+    let t = type || '￥'
+    value = (value * 1).toFixed(num)
+    return t + value
+})
+// 2.0
+const digitsRE = /(\d{3})(?=\d)/g
+Vue.filter('currency', function(value, type = '￥', decimals = 2) {
+    value = parseFloat(value) / 100
+    if (!isFinite(value) || (!value && value !== 0)) {
+        return 0
+    }
+    let stringified = Math.abs(value).toFixed(decimals)
+    let _int = decimals ? stringified.slice(0, -1 - decimals) : stringified
+    let i = _int % 3
+    let head = i > 0 ? (_int.slice(0, i) + (_int.length > 3 ? ',' : '')) : ''
+    let _float = decimals ? stringified.slice(-1 - decimals) : ''
+    let sign = value < 0 ? '-' : ''
+    return sign + type + head + _int.slice(i).replace(digitsRE, '$1,') + _float
+})
+
+/**
+ * 
+ * 状态名称过滤
+ */
+Vue.filter('orderStatus', function(val) {
+    return {
+        '' : '全部',
+        NEW_ORDER: '待支付',
+        SHIPMENTS: '待收货'
+    }
+})
---
+++ vue-install_wxShare.js
@@ -0,0 +1,122 @@
+import Vue from 'vue'
+import axios from 'axios'
+let wxShare = {}
+wxShare.install = function(Vue, options) {
+    // 引入微信SDK
+    const wechatSDK = document.createElement('script')
+    wechatSDK.src = 'http://res.wx.qq.com/open/js/jweixin-1.0.0.js'
+    document.getElementsByTagName('body')[0].appendChild(wechatSDK)
+    
+    let wxSignature = (shareInfo) => {
+        new Vue({
+            data: {
+                shareInfo: {}
+            },
+            created() {
+                axios.post()
+                .then(res => {
+                    // 配置二次分享权限
+                    this.wxConfig(res, shareInfo)
+                })                
+                .catch(error => {})
+            },
+            methods: {
+                wxConfig(res, shareInfo) {
+                    let thumb = shareInfo.thumb
+                    // if (!(thumb.slice(0, 4) === 'http' || thumb.slice(0, 5) === 'https'))
+                    // 配置分享权限
+                    wx.config({
+                        debug: false,
+                        appId: 'wxfdafasfdfsfs', // 必填，公众号的唯一标识
+                        timestamp: res.timestamp, // 必填，生成签名的时间戳
+                        nonceStr: res.nonceStr, // 必填， 生成签名的随机串
+                        signature: res.sign, // 必填，签名
+                        jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone']
+                    })
+                    wx.ready(function() {
+                        // 分享给朋友
+                        wx.onMenuShareAppMessage({
+                            title: shareInfo.title, // 分享标题
+                            desc: shareInfo.intro, // 分享描述
+                            link: shareInfo.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                            imgUrl: thumb, // 分享图标
+                            type: 'link', // 分享类型,music、video或link，不填默认为link
+                            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                            success: function(obj) {
+                                console.log(obj.errMsg);
+                                // 用户确认分享后执行的回调函数
+                            },
+                            fail: function(obj) {
+                                // 用户取消分享后执行的回调函数
+                                alert(obj.errMsg);
+                            }
+                        });
+                        // 分享到朋友圈
+                        wx.onMenuShareTimeline({
+                            title: shareInfo.title, // 分享标题
+                            link: shareInfo.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                            imgUrl: thumb, // 分享图标
+                            success: function (obj) {
+                                // 用户确认分享后执行的回调函数
+                                console.log(obj.errMsg);
+                            },
+                            fail: function (obj) {
+                                // 用户取消分享后执行的回调函数
+                                alert(obj.errMsg);
+                            }
+                        });
+                        //微信分享到QQ
+                        wx.onMenuShareQQ({
+                            title: shareInfo.title, // 分享标题
+                            desc: shareInfo.intro, // 分享描述
+                            link: location.href, // 分享链接
+                            imgUrl: thumb, // 分享图标
+                            success: function (obj) {
+                                // 用户确认分享后执行的回调函数
+                                console.log(obj.errMsg);
+                            },
+                            fail: function (obj) {
+                                // 用户取消分享后执行的回调函数
+                                alert(obj.errMsg);
+                            }
+                        });
+                        // 微信分享到QQ空间
+                        wx.onMenuShareQZone({
+                            title: shareInfo.title, // 分享标题
+                            desc: shareInfo.intro, // 分享描述
+                            link: location.href, // 分享链接
+                            imgUrl: thumb, // 分享图标
+                            success: function (obj) {
+                                // 用户确认分享后执行的回调函数
+                                console.log(obj.errMsg);
+                            },
+                            fail: function (obj) {
+                                // 用户取消分享后执行的回调函数
+                                alert(obj.errMsg);
+                            }
+                        });
+                    })
+                }                 
+            }
+        })
+    }
+    Vue.prototype.$wxShare = {
+        wxShare(shareInfo) {
+            wxSignature(shareInfo)
+        }
+    }
+}
+export default wxShare
+
+
+// 在项目入口处：
+import wxShare from '@/assets/static/wxShare'
+Vue.use(wxShare)
+
+// 在组件中：
+this.$wxShare({
+    "title":share.title,
+    "intro":share.intro,
+    "thumb":share.thumb,
+    "link":location.href
+})
