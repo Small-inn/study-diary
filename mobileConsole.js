b8dd7df2c2d6d119178f1d8cef5139f4e801cc41
Small_inn <small_inn@163.com>
2019-07-22 15:14:43 +0800

    formater

--- mobileConsole.js
+++ mobileConsole.js
@@ -1,6 +1,6 @@
 /**
  * 以下代码放在main.js里面
- * */ 
+ * */
 
 // if (process.env.NODE_ENV === 'production') {
 //   var src = '//cdn.jsdelivr.net/npm/eruda';
@@ -8,3 +8,7 @@
 //   document.write('<scr' + 'ipt src="' + src + '"></scr' + 'ipt>');
 //   document.write('<scr' + 'ipt>eruda.init();</scr' + 'ipt>');
 // }
+
+// 调试
+// import VConsole from 'vconsole/dist/vconsole.min.js' //import vconsole
+// let vConsole = new VConsole() // 初始化
