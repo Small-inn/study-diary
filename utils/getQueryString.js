1e79c9d2e59f619f2df50a72fbde73b510bf69a6
Small-inn <small_inn@163.com>
2020-03-12 14:48:46 +0800

    添加gitignore文件

--- utils/getQueryString.js
+++ utils/getQueryString.js
@@ -4,9 +4,9 @@ const getQueryString = (name) => {
   let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", 'i') // 匹配目标参数
   let result = window.location.search.substr(1).match(reg) // 对querystring匹配目标参数
     if (result != null) {
-        return decodeURIComponent(result[2])
+      return decodeURIComponent(result[2])
     } else {
-        return null
+      return null
     }
 }
 
