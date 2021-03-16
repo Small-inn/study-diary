738eebd4b00aa27e5a7dd7d2ae1984e8489adfc5
liuxie <zyhd.liuxie@waibao.cntaiping.com>
2021-03-16 20:07:19 +0800

    feat: is-inherit

--- javascript/08. js-inherit.js
+++ javascript/08. js-inherit.js
@@ -1,11 +1,42 @@
 /**
  * 继承：
  * 1. 原型继承
- * 2. 组合继承
- * 
+ * 2. 构造函数继承
+ * 3. 组合继承
+ * 4. 原型式继承
+ * 5. 寄生式继承
+ * 6. 寄生式组合继承
  * 
 */
-// ES5
+// 1. 原型继承
+/**
+ * 说明：将子类的原型挂载到父类对象上
+ * 缺点: 子类new出的实例，父类属性没有隔离，会互相影响
+ * 
+*/
+function Person(name, age) {
+    this.name = name
+    this.age = age
+}
+Person.prototype.sayHi = function() {
+    console.log(this.name + '==' + this.age)
+}
+function Sun() {
+    this.type = 'sun'
+}
+Sun.prototype = new Person('lx', 20) // 原型继承
+let sun1 = new Sun()
+let sun2 = new Sun()
+console.log(sun1.name) // lx
+console.log(sun2.age) // 20
+console.log(sun2.sayHi()) // lx==20 
+
+// 2. 构造函数继承 es5 
+/**
+ * 说明：直接利用call或者apply方法将父类构造函数的this绑定为子类构造函数的this就可以了
+ * 缺点：无法继承原型链上的属性与方法
+ * 
+*/
 function Animal(name) {
     this.name = name
 }
@@ -64,3 +95,17 @@ console.log(code1)
 code1.sayName()
 code1.showCompany()
 console.log(code1.sex)
+
+// 3. 组合继承
+/**
+ * 说明：组合上面的构造函数与原型继承的功能
+ * 缺点：call()方法已经拿到父类所有的属性，后面在使用原型时也会有父类的所有属性
+ * 
+*/
+function Person2() {
+    this.name = 'p2'
+    this.arr = [1, 2, 3]
+}
+Person2.prototype.say = function() {
+    console.log(this.name)
+}
