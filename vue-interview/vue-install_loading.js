bb07d22c97cc3295ffc1bd737f8bfc2aecd2827b
Small-inn <1457276774@qq.com>
2023-02-15 13:57:06 +0800

    feat: vue-install-loading.js update

--- vue-interview/vue-install_loading.js
+++ vue-interview/vue-install_loading.js
@@ -1,38 +1,40 @@
 //调用：this.$load.show(颜色值)
 
-var load = {};
-
-load.install = function(Vue) {
-    var load;
-    var _show = (dotColor) => {
-        load = document.getElementById('loading');
-        if (!load) {
-            load = document.createElement('div');
-            load.id = 'loading';
-            document.body.appendChild(load);
-        }
-        new Vue({
-            el: '#loading',
-            template: `
-                <div class="wrapLoad">
-                    <div class="load">
-                        <div :style="dotStyle" class="double-bounce1"></div>
-                        <div :style="dotStyle" class="double-bounce2"></div>
-                    </div>
-                </div>
-            `,
-            data: {
-                dotStyle:{
-                    backgroundColor:dotColor
-                },
+const load = {
+    install: function(Vue) {
+        var load;
+        var _show = (dotColor) => {
+            load = document.getElementById('loading');
+            if (!load) {
+                load = document.createElement('div');
+                load.id = 'loading';
+                document.body.appendChild(load);
             }
-        });
-    }
-   Vue.prototype.$load = {
-        show: (dotColor) => {
-            _show(dotColor);
+            new Vue({
+                el: '#loading',
+                template: `
+                    <div class="wrapLoad">
+                        <div class="load">
+                            <div :style="dotStyle" class="double-bounce1"></div>
+                            <div :style="dotStyle" class="double-bounce2"></div>
+                        </div>
+                    </div>
+                `,
+                data: {
+                    dotStyle:{
+                        backgroundColor:dotColor
+                    },
+                }
+            });
+        }
+        Vue.prototype.$load = {
+            show: (dotColor) => {
+                _show(dotColor);
+            }
         }
     }
 }
 
-export default load;
+// load.install = 
+
+export default load
